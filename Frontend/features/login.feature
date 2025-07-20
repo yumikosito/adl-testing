7c90b12d36fa7f184277b95658689bb530dec0f8
Millaray <millaray.painemil@outlook.cl>
2025-07-19 22:30:32 -0400

    arreglo getPage, prueba extra en login

--- Frontend/features/login.feature
+++ Frontend/features/login.feature
@@ -17,6 +17,11 @@ Feature: Servicio de Login
     And el usuario ingresa "Tester@2025" en el campo de contraseña
 	  Then se debe mostrar una alerta de "Las credenciales proporcionadas son incorrectas"
 
+  Scenario: Intento de ingreso con ambos campos vacios
+	  When el usuario ingresa "" en el campo de email
+    And el usuario ingresa "" en el campo de contraseña
+	  Then se debe mostrar un mensaje de que se requiere llenado de email
+
   Scenario: Intento de ingreso con campo vacío de email
 	  When el usuario ingresa "" en el campo de email
     And el usuario ingresa "Tester" en el campo de contraseña
--- Frontend/pages/GetPage.js
+++ Frontend/pages/GetPage.js
@@ -4,7 +4,6 @@ class GetPage {
     this.selectors = {
       entButton: (page) => page.locator('span:has-text("Entidades")'),
       productsButton: (page) => page.getByRole('link', { name: 'Artículos' }),
-      tableRow:'tr[.cursor-pointer hover:bg-gray-50]'
       
     }
   }
@@ -16,8 +15,7 @@ class GetPage {
   async goToProducts() {
     await this.clickSelector(this.selectors.entButton);
     await this.clickSelector(this.selectors.productsButton);
-    // await this.page.locator('span', { hasText: 'Entidades' }).click();
-    // await this.page.getByRole('link', { name: 'Artículos' }).click();
+    await this.page.waitForTimeout(500)
   }
 
 }
