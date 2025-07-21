905cfa66ecd5bd1fd1d0f25522a00fae8c2c1604
lopezandres07 <andlop17@gmail.com>
2025-07-21 11:21:35 -0400

    feat: add some test to login and create a client to make tests dynamic

--- Backend/test/clients.test.js
+++ Backend/test/clients.test.js
@@ -11,108 +11,150 @@
     }
 */
 
+// Desactiva la verificación de certificados SSL en caso tal
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'
+
 const request = require('supertest')
 require('dotenv').config()
 
 const API_URL = process.env.API_URL
 
+const validUser = {
+  email: 'testeradl@test.com',
+  password: 'Tester@2025',
+}
+
+const uniqueEmail = `user_${Date.now()}@example.com`
+const uniqueCuit = `20-${Math.floor(10000000 + Math.random() * 90000000)}-9`
+
 const validClient = {
   name: 'Andrés López',
-  cuit: '20-12345678-9',
-  email: 'andres.lopez@example.com',
+  cuit: uniqueCuit,
+  email: uniqueEmail,
   phone: '+54 9 11 1234-5678',
   address: 'Av. Corrientes 1234, Buenos Aires, Argentina',
   balance: 15230.75,
   is_active: true,
 }
 
-describe('Validate updating client', () => {
-  it('Should check a valid URL ID', async () => {
-    const response = await request(API_URL)
-      .put(`/clients/not-valid-url`)
-      .send(validClient)
+let createdClientId
 
-    expect(response.status).toBe(404)
-  })
+// Describe general con login provisional
+describe('Client API tests', () => {
+  let token
 
-  it('Should display error messages when send an empty data', async () => {
-    const validClientId = 56
-
-    const response = await request(API_URL)
-      .put(`/clients/${validClientId}`)
-      .send({})
-
-    if (response.status === 200) {
-      throw new Error('El backend no está validando correctamente')
-    }
-
-    expect([400, 422]).toContain(response.status)
-  })
-
-  it('Should validate that the balance of the client is greater or equal to 0', async () => {
-    const clientWithNegativeBalance = { ...validClient, balance: -1 }
-    const validClientId = 56
-
-    const response = await request(API_URL)
-      .put(`/clients/${validClientId}`)
-      .send(clientWithNegativeBalance)
-
-    if (response.status === 200) {
-      throw new Error('El backend no está validando correctamente')
-    }
-
-    expect([400, 422]).toContain(response.status)
-  })
-
-  it('Should return 404 when trying to update a non-existent client', async () => {
-    const nonExistingclientId = 2000
-
-    const response = await request(API_URL)
-      .put(`/clients/${nonExistingclientId}`)
-      .send(validClient)
-
-    expect(response.status).toBe(404)
-  })
-
-  it('Should update an existing client with valid data', async () => {
-    const validClientId = 56
-
-    const updatedClient = {
-      ...validClient,
-      address: 'Nueva dirección 123',
-    }
-
-    const response = await request(API_URL)
-      .put(`/clients/${validClientId}`)
-      .send(updatedClient)
+  beforeAll(async () => {
+    const response = await request(API_URL).post('/login').send(validUser)
 
     expect(response.status).toBe(200)
-    expect(response.body).toHaveProperty('address', 'Nueva dirección 123')
-  })
-})
-
-describe('Validate deleting client', () => {
-  it('Should check a valid URL ID', async () => {
-    const response = await request(API_URL).delete(`/clients/not-valid-url`)
-
-    expect(response.status).toBe(404)
+    token = response.body.access_token
   })
 
-  it('Should return a 404 when trying to delete a non-existent client', async () => {
-    const nonExistingclientId = 2000
+  // Describe con creacion de cliente provisional
+  describe('Validate creation client', () => {
+    it('Should create a client with valid data', async () => {
+      const response = await request(API_URL)
+        .post('/clients')
+        .set('Authorization', `Bearer ${token}`)
+        .send(validClient)
 
-    const response = await request(API_URL).delete(
-      `/clients/${nonExistingclientId}`
-    )
+      expect(response.status).toBe(201)
+      expect(response.body).toHaveProperty('id')
 
-    expect(response.status).toBe(404)
+      createdClientId = response.body.id
+    })
   })
 
-  it('Should delete a client', async () => {
-    const validClientId = 56
+  describe('Validate updating client', () => {
+    it('Should check a valid URL ID', async () => {
+      const response = await request(API_URL)
+        .put(`/clients/not-valid-url`)
+        .set('Authorization', `Bearer ${token}`)
+        .send(validClient)
 
-    const response = await request(API_URL).delete(`/clients/${validClientId}`)
+      expect(response.status).toBe(404)
+    })
 
-    expect(response.status).toBe(204)
+    it('Should display error messages when send an empty data', async () => {
+      const response = await request(API_URL)
+        .put(`/clients/${createdClientId}`)
+        .set('Authorization', `Bearer ${token}`)
+        .send({})
+
+      if (response.status === 200) {
+        throw new Error('El backend no está validando correctamente')
+      }
+
+      expect([400, 422]).toContain(response.status)
+    })
+
+    it('Should validate that the balance of the client is greater or equal to 0', async () => {
+      const clientWithNegativeBalance = { ...validClient, balance: -1 }
+
+      const response = await request(API_URL)
+        .put(`/clients/${createdClientId}`)
+        .set('Authorization', `Bearer ${token}`)
+        .send(clientWithNegativeBalance)
+
+      if (response.status === 200) {
+        throw new Error('El backend no está validando correctamente')
+      }
+
+      expect([400, 422]).toContain(response.status)
+    })
+
+    it('Should return 404 when trying to update a non-existent client', async () => {
+      const nonExistingclientId = 2000
+
+      const response = await request(API_URL)
+        .put(`/clients/${nonExistingclientId}`)
+        .set('Authorization', `Bearer ${token}`)
+        .send(validClient)
+
+      expect(response.status).toBe(404)
+    })
+
+    it('Should update an existing client with valid data', async () => {
+      const updatedClient = {
+        ...validClient,
+        address: 'Nueva dirección 123',
+      }
+
+      const response = await request(API_URL)
+        .put(`/clients/${createdClientId}`)
+        .set('Authorization', `Bearer ${token}`)
+        .send(updatedClient)
+
+      expect(response.status).toBe(200)
+      expect(response.body).toHaveProperty('address', 'Nueva dirección 123')
+    })
+  })
+
+  describe('Validate deleting client', () => {
+    it('Should check a valid URL ID', async () => {
+      const response = await request(API_URL)
+        .delete(`/clients/not-valid-url`)
+        .set('Authorization', `Bearer ${token}`)
+
+      expect(response.status).toBe(404)
+    })
+
+    it('Should return a 404 when trying to delete a non-existent client', async () => {
+      const nonExistingclientId = 2000
+
+      const response = await request(API_URL)
+        .delete(`/clients/${nonExistingclientId}`)
+        .set('Authorization', `Bearer ${token}`)
+
+      expect(response.status).toBe(404)
+    })
+
+    it('Should delete a client', async () => {
+      const response = await request(API_URL)
+        .delete(`/clients/${createdClientId}`)
+        .set('Authorization', `Bearer ${token}`)
+
+      expect(response.status).toBe(204)
+    })
   })
 })
