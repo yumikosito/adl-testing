e7fdc662e2a2c4cb92024b7f34c4f67636239ecf
Millaray <millaray.painemil@outlook.cl>
2025-07-27 17:42:16 -0400

    report backend

--- Backend/jest.config.js
+++ Backend/jest.config.js
@@ -3,6 +3,10 @@ const config = {
   clearMocks: true,
   testEnvironment: 'node',
   testMatch: ['**/test/**/*.test.js'],
+  "reporters": [
+    "default",
+    "jest-html-reporters"
+  ],
 }
 
 module.exports = config
